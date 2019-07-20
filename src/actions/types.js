810c21d391341088b91557d11458010842ae5ba8
jnfrati <nicofrati@gmail.com>
2019-07-20 14:15:01 -0300

    Cleaning directory to apply hooks

--- src/actions/types.js
+++
@@ -1,4 +0,0 @@
-export const NEW_COLA='NEW_COLA'
-export const FETCH_COLAS='FETCH_COLAS'
-export const SET_ACTIVE_CELL='SET_ACTIVE_CELL'
-
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import colasReducer from './colasReducer';
-
-export default combineReducers({
-  colas: colasReducer
-});
